cf971b09941d9174f8063019ce07797206f78d9f
PlasmaXIII <44767290+SantosCJ98@users.noreply.github.com>
2019-04-08 22:26:28 +0200

    Add files via upload

--- src/paquetesej10/Arboles.java
+++ src/paquetesej10/Arboles.java
@@ -8,10 +8,12 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import static teclado.Teclado.*;
 
@@ -44,213 +46,133 @@ public class Arboles {
 		Empleado e9 = new Empleado("Antonio", Categoria.ENCARGADO, LocalDate.of(2011, Month.JANUARY, 28),
 				LocalDate.of(2014, Month.MAY, 14));
 
-		while (opcion != 4) {
+		System.out.println("1. Ordenar por días trabajados\n2. Orden alfabético ascendente\n3. Ordenar por clave");
 
-			System.out.println(
-					"1. Ordenar por días trabajados\n2. Orden alfabético ascendente\n3. Ordenar por clave\n4. Salir");
+		opcion = readRange(1, 3, Rangos.AMBOSIN);
 
-			opcion = readRange(1, 4, Rangos.AMBOSIN);
+		switch (opcion) {
 
-			switch (opcion) {
+		case 1:
 
-			case 1:
+			TreeSet<Empleado> plantilla = new TreeSet<>();
 
-				TreeSet<Empleado> plantilla = new TreeSet<>();
+			plantilla.add(e1);
 
-				plantilla.add(e1);
+			plantilla.add(e2);
 
-				plantilla.add(e2);
+			plantilla.add(e3);
 
-				plantilla.add(e3);
+			plantilla.add(e4);
 
-				plantilla.add(e4);
+			plantilla.add(e5);
 
-				plantilla.add(e5);
+			plantilla.add(e6);
 
-				plantilla.add(e6);
+			plantilla.add(e7);
 
-				plantilla.add(e7);
+			plantilla.add(e8);
 
-				plantilla.add(e8);
+			plantilla.add(e9);
 
-				plantilla.add(e9);
+			plantilla.add(e1);
 
-				plantilla.add(e1);
+			plantilla.add(e5);
 
-				plantilla.add(e5);
+			plantilla.add(e6);
 
-				plantilla.add(e6);
+			for (Empleado empleado : plantilla) {
 
-				for (Empleado empleado : plantilla) {
-
-					System.out.println(empleado.toString());
-
-				}
-
-				break;
-
-			case 2:
-
-				TreeSet<Empleado> plantilla2 = new TreeSet<>();
-
-				plantilla2.add(e1);
-
-				plantilla2.add(e2);
-
-				plantilla2.add(e3);
-
-				plantilla2.add(e4);
-
-				plantilla2.add(e5);
-
-				plantilla2.add(e6);
-
-				plantilla2.add(e7);
-
-				plantilla2.add(e8);
-
-				plantilla2.add(e9);
-
-				plantilla2.add(e1);
-
-				plantilla2.add(e5);
-
-				plantilla2.add(e6);
-
-				List<Empleado> plantilla3 = new ArrayList<>(plantilla2);
-
-				Collections.sort(plantilla3, new Comparator<Empleado>() {
-
-					@Override
-					public int compare(Empleado emp1, Empleado emp2) {
-
-						return emp1.getNombre().compareTo(emp2.getNombre());
-
-					}
-
-				});
-
-				for (Empleado empleado : plantilla3) {
-
-					System.out.println(empleado.toString());
-
-				}
-
-				break;
-
-			case 3:
-
-				TreeSet<Empleado> plantilla4 = new TreeSet<>();
-
-				plantilla4.add(e1);
-
-				plantilla4.add(e2);
-
-				plantilla4.add(e3);
-
-				plantilla4.add(e4);
-
-				plantilla4.add(e5);
-
-				plantilla4.add(e6);
-
-				plantilla4.add(e7);
-
-				plantilla4.add(e8);
-
-				plantilla4.add(e9);
-
-				plantilla4.add(e1);
-
-				plantilla4.add(e5);
-
-				plantilla4.add(e6);
-
-				List<Empleado> plantilla5 = new ArrayList<>(plantilla4);
-
-				Collections.sort(plantilla5, new Comparator<Empleado>() {
-
-					@Override
-					public int compare(Empleado emp1, Empleado emp2) {
-
-						if (emp1.getClave().toString().charAt(0) == 'J'
-								&& emp2.getClave().toString().charAt(0) == 'D') {
-
-							return -1;
-
-						}
-
-						else if (emp1.getClave().toString().charAt(0) == 'J'
-								&& emp2.getClave().toString().charAt(0) == 'E') {
-
-							return -1;
-
-						}
-
-						else if (emp1.getClave().toString().charAt(0) == 'E'
-								&& emp2.getClave().toString().charAt(0) == 'D') {
-
-							return -1;
-
-						}
-
-						else if (emp1.getClave().toString().charAt(0) == 'E'
-								&& emp2.getClave().toString().charAt(0) == 'J') {
-
-							return 1;
-
-						}
-
-						else if (emp1.getClave().toString().charAt(0) == 'D'
-								&& emp2.getClave().toString().charAt(0) == 'J') {
-
-							return 1;
-
-						}
-
-						else if (emp1.getClave().toString().charAt(0) == 'D'
-								&& emp2.getClave().toString().charAt(0) == 'E') {
-
-							return 1;
-
-						}
-
-						else {
-
-							if (emp1.getClave().getNumero() < emp2.getClave().getNumero()) {
-
-								return -1;
-
-							}
-
-							else if (emp1.getClave().getNumero() > emp2.getClave().getNumero()) {
-
-								return 1;
-
-							}
-
-							else {
-
-								return 0;
-
-							}
-
-						}
-
-					}
-
-				});
-
-				for (Empleado empleado : plantilla5) {
-
-					System.out.println(empleado.toString());
-
-				}
-
-				break;
+				System.out.println(empleado.toString());
 
 			}
 
+			break;
+
+		case 2:
+
+			TreeSet<Empleado> plantilla2 = new TreeSet<>();
+
+			plantilla2.add(e1);
+
+			plantilla2.add(e2);
+
+			plantilla2.add(e3);
+
+			plantilla2.add(e4);
+
+			plantilla2.add(e5);
+
+			plantilla2.add(e6);
+
+			plantilla2.add(e7);
+
+			plantilla2.add(e8);
+
+			plantilla2.add(e9);
+
+			plantilla2.add(e1);
+
+			plantilla2.add(e5);
+
+			plantilla2.add(e6);
+
+			List<Empleado> plantilla3 = new ArrayList<>(plantilla2);
+
+			Collections.sort(plantilla3, new Comparator<Empleado>() {
+
+				@Override
+				public int compare(Empleado emp1, Empleado emp2) {
+
+					return emp1.getNombre().compareTo(emp2.getNombre());
+
+				}
+
+			});
+
+			for (Empleado empleado : plantilla3) {
+
+				System.out.println(empleado.toString());
+
+			}
+
+			break;
+
+		case 3:
+
+			Set<Empleado> plantilla4 = new HashSet<>();
+
+			plantilla4.add(e1);
+
+			plantilla4.add(e2);
+
+			plantilla4.add(e3);
+
+			plantilla4.add(e4);
+
+			plantilla4.add(e5);
+
+			plantilla4.add(e6);
+
+			plantilla4.add(e7);
+
+			plantilla4.add(e8);
+
+			plantilla4.add(e9);
+
+			plantilla4.add(e1);
+
+			plantilla4.add(e5);
+
+			plantilla4.add(e6);
+
+			Map<Clave, String> plantilla5 = new TreeMap<>(plantilla4.stream().collect(Collectors.toMap(c -> c.getClave(), v -> v.toString())));
+			
+			for (Map.Entry<Clave, String> plantillaforeach : plantilla5.entrySet()) {
+				
+				System.out.println(plantillaforeach.getValue());
+				
+			}
+			
 		}
 
 	}
--- src/paquetesej10/Clave.java
+++ src/paquetesej10/Clave.java
@@ -1,6 +1,6 @@
 package paquetesej10;
 
-public class Clave {
+public class Clave implements Comparable<Clave> {
 
 	Clave(Categoria categoria, int numero) {
 
@@ -25,6 +25,68 @@ public class Clave {
 	public String toString() {
 
 		return String.format("%c%d", categoria.getCodigo(), (int) getNumero());
+
+	}
+
+	@Override
+	public int compareTo(Clave clave) {
+		if (this.getCategoria().getCodigo() == 'J' && clave.getCategoria().getCodigo() == 'D') {
+
+			return -1;
+
+		}
+
+		else if (this.getCategoria().getCodigo() == 'J' && clave.getCategoria().getCodigo() == 'E') {
+
+			return -1;
+
+		}
+
+		else if (this.getCategoria().getCodigo() == 'E' && clave.getCategoria().getCodigo() == 'D') {
+
+			return -1;
+
+		}
+
+		else if (this.getCategoria().getCodigo() == 'E' && clave.getCategoria().getCodigo() == 'J') {
+
+			return 1;
+
+		}
+
+		else if (this.getCategoria().getCodigo() == 'D' && clave.getCategoria().getCodigo() == 'J') {
+
+			return 1;
+
+		}
+
+		else if (this.getCategoria().getCodigo() == 'D' && clave.getCategoria().getCodigo() == 'E') {
+
+			return 1;
+
+		}
+
+		else {
+
+			if (this.getNumero() < clave.getNumero()) {
+
+				return -1;
+
+			}
+
+			else if (this.getNumero() > clave.getNumero()) {
+
+				return 1;
+
+			}
+
+			else {
+
+				return 0;
+
+			}
+
+		}
 
 	}
 
